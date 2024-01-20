a8ad22b8218a96cec27238ae3d06f3829eaf89da
Robin Mennel <robinmennel88@gmail.com>
2024-01-20 17:20:12 -0600

    Part 2 attempt 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,15 +5,15 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = ""
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? "
-let correctAnswer = "Sally Ride"
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
 let candidateAnswer = ""
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride","true", "40", "Trajectory", "3"];
+let candidateAnswers = []
 
 
 function askForName() {
@@ -24,7 +24,9 @@ candidateName = input.question(`"what is your first and last name? "`)
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 candidateAnswer = input.question(`${question}`)
-
+for (let i =0; i < questions.length; i++){
+  candidateAnswers.push(input.question(questions[i]));
+}
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -34,8 +36,13 @@ if (candidateAnswer === correctAnswer) {
   console.log("Correct. ")
 } else {
   console.log("Wrong. ")
-}
-
+} 
+for (let i = 0; i < questions.length; i++) {
+if (candidateAnswers === candidateAnswers){
+  console.log(`${i + 1}: Correct! \n Your Answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswers[i]}`);
+} else {
+    console.log(`${i + 1}: Wrong. \n Your Answer: ${candidateAnswers[i]} \n Correct Answer${correctAnswers[i]}`);
+}}
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
